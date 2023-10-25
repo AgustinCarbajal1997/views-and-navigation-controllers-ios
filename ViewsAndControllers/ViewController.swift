7e0658ef53dc30fcfe380d3f29a475ad43db1570
AgustinCarbajal1997 <magustincarbajal97@gmail.com>
2023-10-25 20:47:41 -0300

    [feature] map views

--- ViewsAndControllers/ViewController.swift
+++ ViewsAndControllers/ViewController.swift
@@ -1,12 +1,6 @@
-//
-//  ViewController.swift
-//  ViewsAndControllers
-//
-//  Created by Agustin Carbajal on 24/10/2023.
-//
-
 import UIKit
 import WebKit
+import MapKit
 
 class ViewController: UIViewController {
 
@@ -14,6 +8,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var myImageView: UIImageView!
     @IBOutlet weak var myButton: UIButton!
     @IBOutlet weak var myWebView: WKWebView!
+    @IBOutlet weak var myMapView: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,7 +17,7 @@ class ViewController: UIViewController {
         myButton.tintColor = .black
         
         myWebView.load(URLRequest(url: URL(string: "https://google.com")!)) // el ! es como un optional chaining que lo que hace es abortar la ejecucion si contiene un valor nil, corta la ejecucion
-        
+        myMapView.centerCoordinate = CLLocationCoordinate2D(latitude: -24.1744883, longitude: -65.3213312)
     }
 
 
