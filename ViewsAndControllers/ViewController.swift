3acc19e0d0d556ffb636f40b13faf68546e3d66d
AgustinCarbajal1997 <magustincarbajal97@gmail.com>
2023-10-25 20:14:42 -0300

    [feature] stack views

--- ViewsAndControllers/ViewController.swift
+++ ViewsAndControllers/ViewController.swift
@@ -11,12 +11,19 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var myView: UIView!
     @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var myButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        myButton.backgroundColor = .green
+        myButton.tintColor = .black
     }
 
 
+    @IBAction func myButtonAction(_ sender: Any) {
+        myView.isHidden = true
+    }
 }
 
