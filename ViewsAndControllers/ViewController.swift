d89c305513a0e96a19aec6b9fcb030d97ec3c459
AgustinCarbajal1997 <magustincarbajal97@gmail.com>
2023-10-26 19:58:06 -0300

    [feature] navigation controller

--- ViewsAndControllers/ViewController.swift
+++ ViewsAndControllers/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
             
+        title = "Titulo de View Controller"
+        
         myButton.backgroundColor = .green
         myButton.tintColor = .black
         
